a9a2b5cd7a7260e3f3924f10be7269a7c51956d9
encas <encas@riseup.net>
2022-09-27 11:51:05 +0200

    quantié : auto change amount when typing

--- assets/js/quantite.js
+++ assets/js/quantite.js
@@ -1,16 +1,28 @@
-const quantityInput = document.querySelector('#quantityInput'); 
+// réccupéré le nombre d'assiete (quantityInput) et les quantité de base (amounts)
+const quantityInput = document.querySelector("#quantityInput");
+const amounts = document.querySelectorAll(".amount");
+const amountArray = [];
 
-var amount = "";
-const elements = document.querySelectorAll('.amount');
-Array.from(elements).forEach(function(element, index) {
-    amount += " " + element.innerText + " " ;
-});
-
-quantityInput.addEventListener('change', function() { 
-    let quantityValue = quantityInput.value;
-    console.log('quantityInput', quantityValue); 
-    console.log(amount);
-
+// Remplir le tableau des quantité amountArray de chaque quantité signalité comme appartenant a amounts (element de class css .amount)
+Array.from(amounts).forEach(function (element, index) {
+  amountArray.push(Number(element.innerText));
 });
 
 
+// Empecher le rechargement de la page lorsque la touche entrée est presser sur quantityInput
+quantityInput.addEventListener('keydown', function (e) {
+  if(e.keyIdentifier=='U+000A'||e.keyIdentifier=='Enter'||e.keyCode==13)
+  {e.preventDefault();return false;}
+},true);
+
+  
+
+
+// Lorsque quantityInput change, chaque valeur du tableau amountArray est modifié
+  // 'input' agit lorsque la modification se fasse au clavier, à la souris ou autre
+  // 'change' agit lorsque le focus est perdu ; keyup agit lorsqu'une touche du clavier est relaché
+quantityInput.addEventListener("input", function () {
+  amountArray.forEach((value,i) => {
+    amounts[i].innerHTML = (value * quantityInput.value) / 100;
+    });
+  });
